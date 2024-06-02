c14ed753c1e6d7f14406ee07f4aef216d7c83b53
sagarhasan273 <sagarhasan273@gmail.com>
2024-06-03 02:04:51 +0600

    eslint setup done

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "airbnb",
+    "eslint:recommended",
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "prettier"],
+  rules: {
+    quotes: 0,
+    "react/jsx-filename-extension": 0,
+    "react/react-in-jsx-scope": 0,
+    "comma-dangle": 0,
+
+  },
+};
